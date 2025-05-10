eca1f4578abda7e347c7b06f179e4bf976248db3
Amera <amera@gmail.com>
2025-05-10 20:15:35 +0300

    add auth backend

--- server/server.js
+++ server/server.js
@@ -4,18 +4,21 @@ const cors = require('cors');
 require("dotenv").config();
 const connectDB = require("./db/connectDB");
 const { notFound, errorHanlder } = require("./middleware/errors");
+const authRoutes = require("./routes/auth.routes");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // Middleware
 app.use(express.json()); 
+app.use(cookieParser());
 
 app.use(helmet());
 
 app.use(cors());
 
 // Routes
-// app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 // Error Hanlder Middleware
 app.use(notFound);
