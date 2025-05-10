c5eabae09718dce8a71f2544f776e701b6a8e0c0
Amera <amera@gmail.com>
2025-05-10 05:52:02 +0300

    backup setup and connectDB

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const helmet = require("helmet");
+const cors = require('cors');
+require("dotenv").config();
+const connectDB = require("./db/connectDB");
+const { notFound, errorHanlder } = require("./middleware/errors");
+
+const app = express();
+
+// Middleware
+app.use(express.json()); 
+
+app.use(helmet());
+
+app.use(cors());
+
+// Routes
+// app.use("/api/v1/auth", authRoutes);
+
+// Error Hanlder Middleware
+app.use(notFound);
+app.use(errorHanlder);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
