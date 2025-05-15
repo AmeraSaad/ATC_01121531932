67e75f59b7ec212e7b4076cd5b04c8231f8f14e9
Amera <amera@gmail.com>
2025-05-15 17:46:49 +0300

    deploy with vercel

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(helmet());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL , credentials: true }));
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
